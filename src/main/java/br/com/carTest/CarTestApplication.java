285b9ccba2acce7c138f32f3ea0721cfc24cfaf3
Natan Luiz <78290847+NatanLuiz1@users.noreply.github.com>
2023-10-16 15:42:01 -0300

    first commit

---
+++ src/main/java/br/com/carTest/CarTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.carTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carTest/CarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
